948a08cdef2b4b7e6617a35ad13a59e8ac5240f5
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-15 20:47:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,16 +4,16 @@ def countdown(counter)
   while counter > 0
   puts "#{number} SECOND(S)!"
   counter -= 1
-end 
+end
 return "Happy New Year!"
 end
 
 def countdown_with_sleep(counter)
-  while counter > 
+  while counter >
 
 puts "#{counter} SECOND(S)!"
 sleep(1)
 counter -=1
-end 
-return "Happy New Year!"  
+end
+return "Happy New Year!"
 end
